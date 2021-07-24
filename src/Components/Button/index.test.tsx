c1538e98c2ca6e7fb2843c0a04d2290d7b389e91
Colin Park (Hanbin Park) <qkrgksqls9@gmail.com>
2021-07-24 15:49:11 +0900

    Button Testing.2
    1. change color
    2. click event

--- src/Components/Button/index.test.tsx
+++ src/Components/Button/index.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import 'jest-styled-components'
 
 import { Button } from './index'
 
 describe('<Button />', () => {
-  it('renders component correctly', () => {
+  it('올바른 랜더링', () => {
     const { container } = render(<Button label="버튼" />)
 
     const label = screen.getByText('버튼')
@@ -18,5 +18,27 @@ describe('<Button />', () => {
     })
 
     expect(container).toMatchSnapshot()
+  })
+
+  it('background color와 hover color 변경', () => {
+    const backgroundColor = '#d00000'
+    const hoverColor = '#e00000'
+    render(<Button label="삭제" backgroundColor={backgroundColor} hoverColor={hoverColor} />)
+
+    const parent = screen.getByText('삭제').parentElement
+    expect(parent).toHaveStyleRule('background-color', backgroundColor)
+    expect(parent).toHaveStyleRule('background-color', hoverColor, {
+      modifier: ':hover',
+    })
+  })
+
+  it('버튼 클릭', () => {
+    const handleClick = jest.fn()
+    render(<Button label="버튼" onClick={handleClick} />)
+
+    const button = screen.getByText('버튼')
+    expect(handleClick).toHaveBeenCalledTimes(0)
+    fireEvent.click(button)
+    expect(handleClick).toHaveBeenCalledTimes(1)
   })
 })
