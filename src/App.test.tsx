84237f6df8604cf6a9d058bbc77d26fc447f14ab
Colin Park (Hanbin Park) <qkrgksqls9@gmail.com>
2021-07-24 14:42:42 +0900

    <App /> Testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  it('render component correctly', () => {
+    render(<App />);
+
+    const msg = screen.getByText('Hello World!');
+    expect(msg).toBeInTheDocument();
+  });
 });
