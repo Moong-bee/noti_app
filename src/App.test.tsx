00753566b568b7b738e9de433c196cb21193b866
Colin Park (Hanbin Park) <qkrgksqls9@gmail.com>
2021-07-27 01:06:57 +0900

    Clear App
    나중에 백업예정

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,7 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 describe('<App />', () => {
-  it('render component correctly', () => {
-    render(<App />);
-
-    const title = screen.getByText('로그인');
-    expect(title).toBeInTheDocument();
-
-    const emailLogin = screen.getByText('이메일로 로그인');
-    expect(emailLogin).toBeInTheDocument();
-  });
-});
+  it('render component correctly', () => {})
+})
